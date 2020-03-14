bf00188fa66248770edd50b93dba7590ed9d2dce
Kristaps Vitolins <arch.kristaps.vitolins@gmail.com>
2020-03-14 17:25:46 +0200

    Clean up project

--- HotCode.StrongHold/Startup.cs
+++ HotCode.StrongHold/Startup.cs
@@ -15,13 +15,11 @@ namespace HotCode.StrongHold
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -29,11 +27,7 @@ namespace HotCode.StrongHold
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
