a2c19cdeaf6aa35790e84773891e1c6b908f033f
Kristaps Vitolins <kristaps.vitolins@gmail.com>
2020-04-13 03:39:27 +0300

    test tabpe view

---
+++ HotCode.StrongHold.Client/client_app/src/components/table/CustomTable.tsx
@@ -0,0 +1,80 @@
+﻿import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from './customTableStyle';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+
+const useStyles = makeStyles(styles);
+
+interface Props {
+    tableHeaderColor: string;
+    tableHead: Array<string>;
+    tableData: Array<Array<string>>;
+}
+
+const CustomTable: React.FC<Props> = (props) => {
+    const classes = useStyles();
+    const { tableHead, tableData, tableHeaderColor } = props;
+
+    function getMapValue(obj: any, key: any) {
+        if (obj.hasOwnProperty(key)) return obj[key];
+    }
+
+    const tableHeaderColorClass = getMapValue(
+        classes,
+        tableHeaderColor + 'TableHeader'
+    );
+
+    return (
+        <div className={classes.tableResponsive}>
+            <Table className={classes.table}>
+                {tableHead !== undefined ? (
+                    <TableHead className={tableHeaderColorClass}>
+                        <TableRow className={classes.tableHeadRow}>
+                            {tableHead.map((prop, key) => {
+                                return (
+                                    <TableCell
+                                        className={
+                                            classes.tableCell +
+                                            ' ' +
+                                            classes.tableHeadCell
+                                        }
+                                        key={key}
+                                    >
+                                        {prop}
+                                    </TableCell>
+                                );
+                            })}
+                        </TableRow>
+                    </TableHead>
+                ) : null}
+                <TableBody>
+                    {tableData.map((prop, key) => {
+                        return (
+                            <TableRow
+                                key={key}
+                                className={classes.tableBodyRow}
+                            >
+                                {prop.map((prop, key) => {
+                                    return (
+                                        <TableCell
+                                            className={classes.tableCell}
+                                            key={key}
+                                        >
+                                            {prop}
+                                        </TableCell>
+                                    );
+                                })}
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
+
+export default CustomTable;
--- HotCode.StrongHold.Client/client_app/src/views/Role/RolePage.tsx
+++ HotCode.StrongHold.Client/client_app/src/views/Role/RolePage.tsx
@@ -1,9 +1,36 @@
 ﻿import * as React from 'react';
+import { useRoleListQuery } from '../../generated/graphql';
+import CustomTable from '../../components/table/CustomTable';
 
-function RolePage() {
+const RolePage = () => {
+    const { data, error, loading } = useRoleListQuery();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error || !data) {
+        return <div>ERROR</div>;
+    }
+
+    if (!data.roles) {
+        return <div>no roles</div>;
+    }
+
+    const tableData: Array<Array<string>> = [];
+    data.roles.forEach((value) => {
+        if (value) {
+            tableData.push([value.id, value.name, value.description]);
+        }
+    });
+
     return (
-        <div>Role page</div>
-    )
-}
+        <CustomTable
+            tableHeaderColor="primary"
+            tableHead={['Id', 'Name', 'Description']}
+            tableData={tableData}
+        />
+    );
+};
 
 export default RolePage;
